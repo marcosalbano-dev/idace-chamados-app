23ed33a61a2038aaf8ffee68025ecae64264dd42
Marcos Albano <marcos.telematica@gmail.com>
2024-01-22 16:07:47 -0300

    Ajustes nas telas de login e cadastro-usuario

--- src/views/cadastroUsuario.js
+++ src/views/cadastroUsuario.js
@@ -1,8 +1,7 @@
 import React from "react";
-
-import Card from "../components/card";
+import Card from '../components/card'
 import FormGroup from "../components/form-group";
-import '../custom.css'
+
 class CadastroUsuario extends React.Component {
 
     state = {
@@ -12,61 +11,70 @@ class CadastroUsuario extends React.Component {
         senhaRepeticao: ''
     }
 
-
     render() {
         return (
-            <Card title="Cadastro de Usuário">
-                <div className="row">
-                    <div className="col-lg-12">
-                        <div className="bs-component">
-                            <FormGroup label="Nome: *" htmlFor="inputNome">
-                                <input type="text"
-                                    id="inputNome"
-                                    className="form-control"
-                                    name="nome"
-                                    onChange={e => this.setState({ nome: e.target.value })} />
-                            </FormGroup>
-                            <br />
-                            <FormGroup label="Email: *" htmlFor="inputEmail">
-                                <input type="text"
-                                    id="inputEmail"
-                                    className="form-control"
-                                    name="email"
-                                    onChange={e => this.setState({ email: e.target.value })} />
-                            </FormGroup>
-                            <br />
-                            <FormGroup label="Senha: *" htmlFor="inputSenha">
-                                <input type="password"
-                                    id="inputSenha"
-                                    className="form-control"
-                                    name="senha"
-                                    onChange={e => this.setState({ senha: e.target.value })} />
-                            </FormGroup>
-                            <br />
-                            <FormGroup label="Repita a Senha: *" htmlFor="inputRepitaSenha">
-                                <input type="password"
-                                    id="inputRepitaSenha"
-                                    className="form-control"
-                                    name="senha"
-                                    onChange={e => this.setState({ senhaRepeticao: e.target.value })} />
-                            </FormGroup>
-                            <br />
-                            <button
-                                type="button"
-                                className="btn btn-success">
-                                <i className="pi pi-save"></i>
-                                Salvar</button>&nbsp;
-                            <button
-                                type="button"
-                                className="btn btn-danger">
-                                <i className="pi pi-times"></i>
-                                Cancelar</button>
-                        </div>
+            <div className="row" >
+                <div className="col-md-6 offset-md-3">
+                    <div className="bs-docs-section">
+                        <Card title="Cadastro de Usuário">
+                            <div className="row">
+                                <div className="col-lg-12">
+                                    <div className="bs-component">
+                                        <fieldset>
+                                        <FormGroup label="Nome: *" htmlFor="inputNome">
+                                            <input type="text"
+                                                id="inputNome"
+                                                className="form-control"
+                                                name="nome"
+                                                onChange={e => this.setState({ nome: e.target.value })} />
+                                        </FormGroup>
+                                        <br />
+                                        <FormGroup label="Email: *" htmlFor="inputEmail">
+                                            <input type="text"
+                                                id="inputEmail"
+                                                className="form-control"
+                                                name="email"
+                                                onChange={e => this.setState({ email: e.target.value })} />
+                                        </FormGroup>
+                                        <br />
+                                        <FormGroup label="Senha: *" htmlFor="inputSenha">
+                                            <input type="password"
+                                                id="inputSenha"
+                                                className="form-control"
+                                                name="senha"
+                                                onChange={e => this.setState({ senha: e.target.value })} />
+                                        </FormGroup>
+                                        <br />
+                                        <FormGroup label="Repita a Senha: *" htmlFor="inputRepitaSenha">
+                                            <input type="password"
+                                                id="inputRepitaSenha"
+                                                className="form-control"
+                                                name="senha"
+                                                onChange={e => this.setState({ senhaRepeticao: e.target.value })} />
+                                        </FormGroup>
+                                        <br />
+                                        <button onClick={this.cadastrar}
+                                            type="button"
+                                            className="btn btn-success">
+                                            <i className="pi pi-save"></i>
+                                            Salvar</button>&nbsp;
+                                        <button onClick={this.cancelar}
+                                            type="button"
+                                            className="btn btn-danger">
+                                            <i className="pi pi-times"></i>
+                                            Cancelar</button>
+                                        </fieldset>
+                                    </div>
+                                </div>
+                            </div>
+                        </Card>
                     </div>
                 </div>
-            </Card>
+            </div>
         )
     }
 }
+
+// Login.contextType = AuthContext;
 
 export default CadastroUsuario
